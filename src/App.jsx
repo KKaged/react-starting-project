23fbd0a86ea3740b3c502684786b8ceff504a04b
KKaged <dhigaredaleon707@gmail.com>
2023-12-12 18:05:47 +0900

    learn how to properly display data

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,26 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TapButton from './components/TapButton.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components") //Initial state is components
+  const [selectedTopic, setSelectedTopic] = useState() //Initial state is components
   //useState basically re runs the App function when the state/page is changed
   function handleSelect(selectedButton){
     //selectedButton => 'components', 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton)
+  }
+  let tabContent = <p>Click on a topic.</p>
+  if(selectedTopic){
+    tabContent =(
+    <div id="tab-content">
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    {/* Inside the brackets is the name of the chosen object */}
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>
+        {EXAMPLES[selectedTopic].code}
+      </code>
+    </pre>
+  </div>
+    )
   }
   return (
     <div>
@@ -33,16 +48,21 @@ function App() {
             <TapButton onSelect={() => handleSelect(`props`)}>Props</TapButton>
             <TapButton onSelect={() => handleSelect(`state`)}>State</TapButton>
           </menu>
+          {tabContent}
+          {!selectedTopic ? (<p>Click on a topic.</p>) : (
           <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            {/* Inside the brackets is the name of the chosen object */}
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          {/* Inside the brackets is the name of the chosen object */}
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+              {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+        </div>
+          )}
+          {/* If the selectedTopic is empty, then it will show the first part of the ternary operator */}
+          {/* If the selectedTopic is not empty, then it will show the second part of the ternary operator */}
         </section>
       </main>
     </div>
