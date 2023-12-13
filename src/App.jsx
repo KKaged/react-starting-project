bf3073c3a53838486cc2e5086fd7680e259890c6
KKaged <dhigaredaleon707@gmail.com>
2023-12-13 21:03:09 +0900

    learned about key and how to display using the .map method

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TapButton from './components/TapButton.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState() //Initial state is components
+  const [selectedTopic, setSelectedTopic] = useState() //Initial state is empty (false)
   //useState basically re runs the App function when the state/page is changed
   function handleSelect(selectedButton){
     //selectedButton => 'components', 'jsx', 'props', 'state'
@@ -34,10 +34,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            {CORE_CONCEPTS.map((conceptItem)=> <CoreConcept key={conceptItem.title}{...conceptItem}/>)} 
+            {/*Executes once for every item in the array and returns it */}
+            {/* Key is a unique id to be able to tell these different items apart */}
           </ul>
         </section>
         <section id='examples'>
@@ -47,7 +46,7 @@ function App() {
             <TapButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect(`jsx`)}>JSX</TapButton>
             <TapButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect(`props`)}>Props</TapButton>
             <TapButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect(`state`)}>State</TapButton>
-          </menu>
+          </menu>{/*The isSelected is a boolean value, so it will either be true or false */}
           {tabContent}
           {!selectedTopic ? (<p>Click on a topic.</p>) : (
           <div id="tab-content">
