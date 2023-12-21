824a670085355433b0ff852d5f19571597352832
KKaged <dhigaredaleon707@gmail.com>
2023-12-21 22:39:29 +0900

    reviewed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { EXAMPLES } from './data.js'
 import Header from './components/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx';
 import TapButton from './components/TapButton.jsx';
+import { ClassNames } from '@emotion/react';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState() //Initial state is empty (false)
@@ -42,26 +43,12 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TapButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect(`components`)}>Components</TapButton>
-            <TapButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect(`jsx`)}>JSX</TapButton>
-            <TapButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect(`props`)}>Props</TapButton>
-            <TapButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect(`state`)}>State</TapButton>
+            <TapButton isSelected={selectedTopic === 'components'} onSelect={handleSelect}>Components</TapButton>
+            <TapButton>JSX</TapButton>
+            <TapButton>Props</TapButton>
+            <TapButton>State</TapButton>
           </menu>{/*The isSelected is a boolean value, so it will either be true or false */}
-          {tabContent}
-          {!selectedTopic ? (<p>Click on a topic.</p>) : (
-          <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          {/* Inside the brackets is the name of the chosen object */}
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-              {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-        </div>
-          )}
-          {/* If the selectedTopic is empty, then it will show the first part of the ternary operator */}
-          {/* If the selectedTopic is not empty, then it will show the second part of the ternary operator */}
+
         </section>
       </main>
     </>
