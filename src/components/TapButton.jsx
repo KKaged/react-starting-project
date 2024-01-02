1f628cd00452965ba91d5c5b4458ff964848a7d4
KKaged <dhigaredaleon707@gmail.com>
2024-01-02 10:54:40 +0900

    more review for rendering conditional content

--- src/components/TapButton.jsx
+++ src/components/TapButton.jsx
@@ -1,7 +1,10 @@
 export default function TapButton({children, onSelect, isSelected}){
     return(
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+            <button
+             className={isSelected ? 'active' : undefined} 
+             onClick={onSelect}>{children}
+            </button>
         </li>
     )
 }
