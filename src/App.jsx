23bd0c424b4ce7a1ec69a8104d667237c8c6fb16
KKaged <dhigaredaleon707@gmail.com>
2024-01-02 10:24:53 +0900

    more review for rendering conditional content

--- src/App.jsx
+++ src/App.jsx
@@ -30,18 +30,21 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TapButton onSelect={() =>handleSelect('components')}>Components</TapButton>
-            <TapButton onSelect={() =>handleSelect('jsx')}>JSX</TapButton>
-            <TapButton onSelect={() =>handleSelect('props')}>Props</TapButton>
-            <TapButton onSelect={() =>handleSelect('state')}>State</TapButton>
+            <TapButton isSelected={selectedTopic === 'components'} onSelect={() =>handleSelect('components')}>Components</TapButton>
+            <TapButton isSelected={selectedTopic === 'jsx'} onSelect={() =>handleSelect('jsx')}>JSX</TapButton>
+            <TapButton isSelected={selectedTopic === 'props'} onSelect={() =>handleSelect('props')}>Props</TapButton>
+            <TapButton isSelected={selectedTopic === 'state'} onSelect={() =>handleSelect('state')}>State</TapButton>
           </menu>{/*The isSelected is a boolean value, so it will either be true or false */}
             {!selectedTopic ? <p>Please select a topic.</p> : null}
-            {selectedTopic ? (<div id="tab-content"> <h3>{EXAMPLES[selectedTopic].title}</h3>
+            {selectedTopic ? (
+            <div id="tab-content"> 
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
               <code>{EXAMPLES[selectedTopic].code}</code>
             </pre>
-            </div>) : null}
+            </div>
+            ) : null}
             {/* If the selectedTopic is true, then it will display the content, if not, it will display nothing */}
         </section>
       </main>
